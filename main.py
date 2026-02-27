294d9ef912467bea9efcfc1a706e3d99898a4c57
fancypantsonemillion <ravi.falsetti@gmail.com>
2026-02-27 00:54:09 -0500

    added parser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,14 +7,24 @@ from google import genai
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    parser = argparse.ArgumentParser(description="AI Agent")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
 
     print("Hello from ai-agent!")
     if api_key is None:
         raise RuntimeError("api key not found")
 
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    print(response.text)
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.user_prompt)
+    if response.usage_metadata is None:
+        raise RuntimeError("API request failed: No usage metadata")
+    
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
