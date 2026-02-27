ebe83f4907f550909512a438a3564d99b0c6b58c
fancypantsonemillion <ravi.falsetti@gmail.com>
2026-02-27 08:49:55 -0500

    Added tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,7 +9,7 @@ def get_files_info(working_directory, directory="."):
         if os.path.commonpath([working_dir_abs, target_dir]) != working_dir_abs:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         
-        if not os.path.isdir(directory):
+        if not os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
         
         items = os.listdir(target_dir)
@@ -23,7 +23,7 @@ def get_files_info(working_directory, directory="."):
 
             output.append(f"-{item}: file_size={size} bytes, is_dir={is_dir}")
         
-        return "/n".join(output)
+        return "\n".join(output)
     
     except Exception as e:
         return f"Error:{str(e)}"
